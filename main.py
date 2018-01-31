6628640cc87f8304ebca4d9bafae8b6ab01a3fca
evanreilly <ereilly1@uvm.edu>
2018-01-31 15:02:14 -0500

    working colors

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 
 
 
+from colorama import init
+from colorama import Fore, Back, Style
+from termcolor import colored
+from pyautogui import press, typewrite, hotkey
+
+press('a')
+typewrite('quick brown fox')
+hotkey('ctrl', 'w')
+# use Colorama to make Termcolor work on Windows too
+init()
+
+# # then use Termcolor for all colored text output
+# print(colored('Hello, World!', 'green', 'on_red'))
+# print(colored('O', 'cyan'))
+# print(colored('O', 'red')+colored('O','cyan'))
 
 
-def main: 
-	print("Hello World")
-	print("Hello again")
-	print(2+2)
-main()
+# print(Fore.RED + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.BRIGHT + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+
+
+def printBall(color):
+	print(Style.BRIGHT + colored("O", color),end="")
+	
+printBall("red")
+print("")
+printBall("cyan")
+printBall("green")
+printBall("white")
+print(Style.DIM+colored('Some bright red text',"red"))
+#printBall()
+print(Fore.RED+Style.BRIGHT+'Some bright red text')
+
+person = input('Enter your name: ')
+print('Hello', person)
