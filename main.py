e7241631835a03e825224a2404657a4fbe433ade
fierypianist <ajray@uvm.edu>
2018-02-07 15:40:02 -0500

    sudo code

--- main.py
+++ main.py
@@ -39,3 +39,24 @@ print(Fore.RED+Style.BRIGHT+'Some bright red text')
 
 person = input('Enter your name: ')
 print('Hello', person)
+
+
+# response
+# red = 1 right color in right place
+# white = 1 right color in wrong place
+
+# Start screen
+
+# welcome to mastermind!
+# select your gametype
+# 	user guesses 
+
+#		generate a code from the computer of 4 colors in 4 spots
+#		user gets 10 tries to guess colors/orders
+#		input guesses with letter in specific position
+#		feedback: response (see above)
+
+# 	computer guesses
+#		user comes up with a code for the computer to guess
+#		computer gets 10 tries to guess the code
+# 		feedback
